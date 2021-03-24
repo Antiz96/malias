b504c6a9524c74a85fc316f56efc3b47a2fd8270
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-03-24 19:26:08 +0100

    Lastest Release

--- install_alias_manager.sh
+++ install_alias_manager.sh
@@ -2,10 +2,15 @@
 
 echo ""
 mkdir ~/.alias_manager && echo "Alias manager directory successfully created"
+
 curl -s https://raw.githubusercontent.com/Antiz96/alias_manager/master/alias_manager.sh --output alias_manager.sh && mv alias_manager.sh ~/.alias_manager/ && echo "Alias manager program successfully downloaded"
+
 curl -s https://raw.githubusercontent.com/Antiz96/alias_manager/master/update_alias_manager.sh --output update_alias_manager.sh && mv update_alias_manager.sh ~/.alias_manager/ && echo "Update script successfully downloaded"
+
 curl -s https://raw.githubusercontent.com/Antiz96/alias_manager/master/uninstall_alias_manager.sh --output uninstall_alias_manager.sh && mv uninstall_alias_manager.sh ~/.alias_manager/ && echo "Uninstallation script successfully downloaded"
+
 chmod +x ~/.alias_manager/*.sh && echo "Alias manager program succesfully configured"
+
 echo "alias alias_manager='source ~/.alias_manager/alias_manager.sh'" >> ~/.bashrc && source ~/.bashrc && echo "Specific alias successfully created"
 
 rm -f install_alias_manager.sh
