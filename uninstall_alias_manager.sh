816409397cc530a66ad01897e16391401984e11d
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2022-04-29 12:36:50 +0200

    Delete uninstall_alias_manager.sh

--- uninstall_alias_manager.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i "/alias alias_manager='source ~\/.alias_manager\/alias_manager.sh'/d" ~/.bashrc && source ~/.bashrc && unalias alias_manager && rm -rf ~/.alias_manager && echo -e "\nThe Alias Manager program has been successfully uninstalled" 
