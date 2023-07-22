3955fbf5c528d8a0e510658497613bf5d684633d
Robin Candau <robincandau@protonmail.com>
2023-07-22 16:06:11 +0200

    Add filter to the mdl job in CI

--- .github/workflows/mdl_style.rb
+++ .github/workflows/mdl_style.rb
@@ -4,5 +4,5 @@ all
 # Exclude the rules that refers to the line length
 exclude_tag :line_length
 
-# Set accepted trailing spaces to a maximum of 2 (used to break a line) for MD009 rule
+# Set accepted trailing spaces to a maximum of 2 (used to break a line) for the MD009 rule
 rule 'MD009', :br_spaces => 2
